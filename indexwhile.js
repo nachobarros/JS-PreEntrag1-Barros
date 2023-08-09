2c573fdfbd7a929a98a807c15952f97ed85a50ad
nacho barros <jignaciobarrosp@gmail.com>
2023-08-09 14:02:12 -0400

    primer avance

---
+++ indexwhile.js
@@ -0,0 +1,18 @@
+const numeroMesa = parseFloat(prompt('Ingrese número de mesa'))
+
+const platoEntrada = prompt('Nombre plato de entrada')
+const precioPlatoEntrada = parseFloat(prompt('Ingrese precio sin puntos ni signos'))
+
+const platoPrincipal = prompt('Nombre de plato principal')
+const precioPlatoPrincipal = parseFloat(prompt('Ingrese precio sin puntos ni signos'))
+
+const ensalada = prompt('Ingrese nombre de la ensalada')
+const precioEnsalada = parseFloat(prompt('Ingrese precio sin puntos ni signos'))
+
+const bebestible = prompt('Ingrese nombre de bebestible consumido')
+const precioBebestible = parseFloat(prompt('Ingrese precio sin puntos ni signos'))
+
+const postre = prompt ('Ingrese nombre de postre')
+const precioPostre = parseFloat(prompt('Ingrese precio sin puntos ni signos'))
+
+
