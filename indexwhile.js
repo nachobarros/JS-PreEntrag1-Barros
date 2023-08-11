fff331afaacbd47c1d8bab88e2c9cc37472130d7
nacho barros <jignaciobarrosp@gmail.com>
2023-08-11 15:16:06 -0400

    primera entrega lista

--- indexwhile.js
+++ indexwhile.js
@@ -1,3 +1,4 @@
+
 const numeroMesa = parseFloat(prompt('Ingrese número de mesa'))
 
 const platoEntrada = prompt('Nombre plato de entrada')
@@ -14,5 +15,21 @@ const precioBebestible = parseFloat(prompt('Ingrese precio sin puntos ni signos'
 
 const postre = prompt ('Ingrese nombre de postre')
 const precioPostre = parseFloat(prompt('Ingrese precio sin puntos ni signos'))
+
+total = (precioPlatoEntrada + precioPlatoPrincipal + precioEnsalada + precioBebestible + precioPostre)  
+
+const propina = prompt('¿Desea agregar propina?')
+if (propina == 'si'){
+    (totalfinal = total * 1.1)
+} else {
+    (totalfinal = total * 1)
+}
+
+const resumen = prompt('¿Desea el resumen de la mesa?')
+if (resumen == 'si') {
+    alert('Mesa número ' + (numeroMesa) + ', ' + '-Plato entrada: ' + (platoEntrada) + ' $'+ (precioPlatoEntrada) + ', ' + '-Plato Principal: ' + (platoPrincipal) + ' $'+ (precioPlatoPrincipal) + ', ' + '-Plato Ensalada: ' + (ensalada) + ' $'+ (precioEnsalada) + ', ' + '-Bebestible: ' + (bebestible) + ' $'+ (precioBebestible) + ', ' + '-Postre: ' + (postre) + ' $'+ (precioPostre) + '; El total asciende a $ ' + (totalfinal) + ' pesos argentinos')
+} else {
+    alert('El total de la mesa ' + (numeroMesa)  +  ' son ' + '' + (parseInt(totalfinal)) + ' pesos agentinos')
+}
 
 
